9010a59c971af0b20444083295a23b1d7f92ab27
deepak <deepakyadav.hestabit@gmail.com>
2022-04-19 18:40:40 +0530

    add new role for mongodb and nginx

---
+++ Ansible_playbook/nginx/download_roles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ ! -e geerlingguy.nginx ]]; then
+    ansible-galaxy install -p ${PWD} geerlingguy.nginx
+elif [[ ! -d geerlingguy.nginx ]]; then
+    echo "geerlingguy.nginx already exist but is not a directory" 1>&2
+fi
+
+cat << 'EOF' > ${PWD}/site.yaml 
+---
+- hosts: localhost
+  become: yes
+  roles:
+  - geerlingguy.nginx
+EOF
